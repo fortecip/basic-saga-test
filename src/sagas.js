944f864c7f1b25e245fc3dcc9776e0823eb5eb2d
Ciprian Turcu <ciprian.turcu@fortech.ro>
2019-04-02 09:57:17 +0300

    finished saga test

--- src/sagas.js
+++ src/sagas.js
@@ -5,12 +5,8 @@ export function* testRequest() {
   yield axios.get('https://jsonplaceholder.typicode.com/todos').then((response) => {
     title = response.data[0].title;
     // console.log('title',title);
-
   });
   yield put({ type: 'REQUEST_ASYNC', title });
-
-
-
 }
 export function* watchTestRequest() {
   yield takeEvery('REQUEST', testRequest);
